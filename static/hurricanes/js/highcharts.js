f0782c116be79184753952f7f8131f9850de3226
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-10-31 12:36:40 -0700

    adding stuff I've been working on

---
+++ static/hurricanes/js/highcharts.js
@@ -0,0 +1,12 @@
+/**
+ * Calculate the data output
+ */
+// function getData(year) {
+
+//     let output = initialData.map(data => {
+//       return [data["Country Name"], data[year]]
+//     }).sort((a, b) => b[1] - a[1]);
+  
+//     return ([output[0], output.slice(1, 11)]);
+//   }
+
